3418434f3dd3316759eaef4669ad9c1815569f2e
jameshamann <jameshamann@me.com>
2017-06-08 20:57:32 +0100

    sets up items table in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170608195252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string  "name"
+    t.text    "description"
+    t.integer "price"
+  end
 
 end
