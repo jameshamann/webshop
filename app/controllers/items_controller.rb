5471f5c5be37089bda30fa7fe3cab8b4f7033f7e
jameshamann <jameshamann@me.com>
2017-06-11 19:22:53 +0100

    allows a user to add items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,26 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+      @item = Item.create(item_params)
+      if @item.save
+        redirect_to items_path
+        flash[:notice] = "Item successfully added!"
+      else
+      render "new"
+      flash[:alert] = "Sorry, you must add a valid item"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
+
+
 end
--- spec/features/adding_an_item_spec.rb
+++ spec/features/adding_an_item_spec.rb
@@ -6,8 +6,20 @@ feature 'items' do
 
     it 'Shows no Items are available and allows a user to add items' do
       visit '/items'
-      expect(page).to have_content("No items available")
-      expect(page).to have_link("Add Item")
+      expect(page).to have_content("No items yet!")
+      expect(page).to have_link("Add a new Item")
+    end
+
+    it 'Allows a user to add items' do
+      visit '/items'
+      click_link ("Add a new Item")
+      expect(current_path).to eq "/items/new"
+      fill_in 'item_name', with: 'Sample Kit'
+      fill_in 'item_description', with: 'A Sampling Kit to be used for sample analysis'
+      fill_in 'item_price', with: '10'
+      click_button 'Create Item'
+      expect(current_path).to eq '/items'
+      expect(page).to have_content 'Sample Kit'
     end
   end
 end
