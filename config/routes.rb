7df664bc372206f5375b4bcdd9b1a8bb88750eb4
jameshamann <jameshamann@me.com>
2017-06-20 12:45:06 +0100

    generates devise custom user models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "application#index"
-  resources :items 
+  resources :items
+
+  Rails.application.routes.draw do
+     devise_for :users, controllers: {
+       sessions: 'users/sessions'
+     }
+   end
 end
