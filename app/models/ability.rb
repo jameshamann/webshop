3a52730ba81ed5d0e86c57d9f830d12c865c8537
jameshamann <jameshamann@me.com>
2017-06-21 10:22:11 +0100

    only allows superadmin users to access admin page

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.superadmin_role?
+          can :manage, :all
+          can :access, :rails_admin       # only allow admin users to access Rails Admin
+          can :dashboard                  # allow access to dashboard
+    end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
