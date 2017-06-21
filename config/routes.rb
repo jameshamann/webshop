bb5f1d0c558ea8de583880a04a48edf19c9e44c1
jameshamann <jameshamann@me.com>
2017-06-21 09:56:00 +0100

    fixes errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "application#index"
   resources :items
-
-  Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
      devise_for :users, controllers: {
        sessions: 'users/sessions',
        passwords: 'users/passwords',
        registrations: 'users/registrations'
      }
-   end
 end
