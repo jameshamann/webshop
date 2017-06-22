a67172a0e2e460394317bd44ce17ecff8ca4f80f
jameshamann <jameshamann@me.com>
2017-06-22 17:04:12 +0100

    refactor tests

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -6,15 +6,6 @@ def sign_in_admin(email: "test@example.com", password: "12345678", superadmin_ro
   click_button('Log in')
 end
 
-def sign_up_admin(email: "test@example.com", password: "12345678", password_confirmation: "12345678", superadmin_role: true)
-  visit('/')
-  click_link('Sign up')
-  fill_in('user_email', with: email)
-  fill_in('user_password', with: password)
-  fill_in('user_password_confirmation', with: password)
-  click_button('Sign Up')
-end
-
 def add_item(name: "Sample Kit", description: "A Sampling Kit to be used for sample analysis", price: "10")
   visit ('/items')
   click_link ("Add a new Item")
