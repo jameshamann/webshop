9f3f39c540a2e05e92f63bbe638b358c282688e8
jameshamann <jameshamann@me.com>
2017-06-23 14:32:05 +0100

    adds new tables for order status and basket contents, adds mock data to seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622112403) do
+ActiveRecord::Schema.define(version: 20170623132047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,35 @@ ActiveRecord::Schema.define(version: 20170622112403) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "order_id"
+    t.decimal  "unit_price",  precision: 12, scale: 3
+    t.integer  "quantity"
+    t.decimal  "total_price", precision: 12, scale: 3
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id", using: :btree
+    t.index ["order_id"], name: "index_order_items_on_order_id", using: :btree
+  end
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal  "subtotal",        precision: 12, scale: 3
+    t.decimal  "tax",             precision: 12, scale: 3
+    t.decimal  "shipping",        precision: 12, scale: 3
+    t.decimal  "total",           precision: 12, scale: 3
+    t.integer  "order_status_id"
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.index ["order_status_id"], name: "index_orders_on_order_status_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +77,7 @@ ActiveRecord::Schema.define(version: 20170622112403) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "order_statuses"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,13 @@ user.password = 'testing'
 user.password_confirmation = 'testing'
 user.superadmin_role = true
 user.save!
+
+Item.create! id: 1, name: "Banana", price: 25, description: 'This is mock data', avatar_file_name: 'startup-593329_1920.jpg'
+Item.create! id: 2, name: "Apple", price: 40, description: 'This is mock data', avatar_file_name: 'startup-593329_1920.jpg'
+Item.create! id: 3, name: "Carton of Strawberries", price: 10.99, description: 'This is mock data', avatar_file_name: 'startup-593329_1920.jpg'
+
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "In Progress"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled" 
