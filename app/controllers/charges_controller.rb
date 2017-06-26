03946fb906d61125c0d65626cc870fcdc9573083
jameshamann <jameshamann@me.com>
2017-06-26 15:27:29 +0100

    refactoring

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,25 +1,39 @@
 class ChargesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :amount_to_be_charged
   layout "application"
 
-  def create
-    customer = StripeTool.create_customer(email: params[:stripeEmail], stripe_token: params[:stripeToken])
-    charge = StripeTool.create_charge(customer_id: customer.id, amount: @amount, description: @description)
+  before_action :authenticate_user!
+  before_action :amount_to_be_charged
+  before_action :description
 
-  redirect_to thanks_path
+  def new
+  end
+
+  def create
+    
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => @description,
+      :currency    => 'usd'
+    )
+
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
-  end
-
-  def thanks
-
   end
 
   private
     def amount_to_be_charged
       current_order = Order.find(session[:order_id])
       @amount = current_order.subtotal * 100
+    end
+
+    def description
+      @description = "Sample Logistics Webshop"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,11 @@ Rails.application.routes.draw do
   #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :charges
-
-  get 'thanks', to: 'charges#thanks', as: 'thanks'
-
   resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy]
+
+  resources :charges
+
 
   root "application#index"
   resources :items
